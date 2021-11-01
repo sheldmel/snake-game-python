dcc37d7b73dcdfeef39b194eef70fbe9b7927614
unknown <dmello.shelton@yahoo.com>
2021-11-01 11:12:08 -0400

    added Game and Snake classes

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 import pygame
 from pygame.locals import *
+from classes import Game
 
 if __name__ == "__main__":
-    pygame.init()
+    game = Game()
+    game.run()
 
-    surface = pygame.display.set_mode((500, 500))
-    surface.fill((110,110,5))
-    pygame.display.flip()
-
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                pass
-            elif event.type == QUIT:
-                running = False
