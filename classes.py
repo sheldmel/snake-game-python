87c6e836224d1a0408b3003023e2e6898bd8fd5b
unknown <dmello.shelton@yahoo.com>
2021-11-01 11:30:38 -0400

    Added apple class and implemented draw method in snake class

--- classes.py
+++ classes.py
@@ -1,14 +1,20 @@
 import pygame
 from pygame.locals import *
-
+import time
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((1000, 500))
+        self.surface = pygame.display.set_mode((1000, 800))
         self.surface.fill((110, 110, 5))
-        self.snake = Snake(self.surface)
+        self.snake = Snake(self.surface, 2)
         self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
+
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
 
     def run(self):
         running = True
@@ -33,31 +39,66 @@ class Game:
                 elif event.type == QUIT:
                     running = False
 
+            self.play()
+            time.sleep(0.3)
+
+SIZE = 40
+
+
+class Apple:
+    def __init__(self, parent_screen):
+        self.image = pygame.image.load('resources/apple.jpg').convert()
+        self.parent_screen = parent_screen
+        self.x = SIZE * 3
+        self.y = SIZE * 3
+
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
 
 class Snake:
-    def __init__(self, parent_screen):
+
+    def __init__(self, parent_screen, length):
         self.parent_screen = parent_screen
+        self.length = length
         self.block = pygame.image.load('resources/block.jpg').convert()
-        self.x = 100
-        self.y = 100
+        self.x = [SIZE] * length
+        self.y = [SIZE] * length
+        self.direction = 'down'
 
     def draw(self):
         self.parent_screen.fill((110, 110, 5))
-        self.parent_screen.blit(self.block, (self.x, self.y))
-        pygame.display.flip()
+        for i in range(self.length):
+            self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
+            pygame.display.flip()
 
     def move_left(self):
-        self.x -=10
-        self.draw()
+        self.direction = 'left'
 
     def move_right(self):
-        self.x += 10
-        self.draw()
+        self.direction = 'right'
 
     def move_up(self):
-        self.y -= 10
-        self.draw()
+        self.direction = 'up'
 
     def move_down(self):
-        self.y += 10
+        self.direction = 'down'
+
+    def walk(self):
+        for i in range(self.length-1,0,-1):
+            self.x[i] = self.x[i-1]
+            self.y[i] = self.y[i-1]
+
+        if self.direction == "up":
+            self.y[0] -= SIZE
+
+        if self.direction == "down":
+            self.y[0] += SIZE
+
+        if self.direction == "left":
+            self.x[0]  -= SIZE
+
+        if self.direction == "right":
+            self.x[0] += SIZE
+
         self.draw()
